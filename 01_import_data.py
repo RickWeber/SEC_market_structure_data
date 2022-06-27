21833b0661781d9dd22e0199d552a2e767805464
RickWeber <863114+RickWeber@users.noreply.github.com>
2022-06-27 14:00:27 -0400

    add functionality to extract double-zipped data

--- 01_import_data.py
+++ 01_import_data.py
@@ -17,6 +17,11 @@ def link_to_df(link):
     r = requests.get(link)
     b = io.BytesIO(r.content)
     z= zipfile.ZipFile(b)
+    # see if the hoped for contents are there, and if not, dig into any folders in here.
+    if any(".zip" in f.filename for f in z.filelist):
+        path = z.extract(z.namelist()[0])
+        z = zipfile.ZipFile(path)
+    print([f.filename for f in z.filelist])
     keep = [f.filename for f in z.filelist if re.search('csv', f.filename)][0]
     path = z.extract(keep)
     return pd.read_csv(path)
@@ -26,3 +31,5 @@ df = link_to_df(zip_base + zip_links[0])
 for z in zip_links[1:]:
     this_df = link_to_df(zip_base + z)
     df = pd.concat([df, this_df])
+
+df.sample(10)
