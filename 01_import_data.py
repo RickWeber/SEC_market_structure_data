eed49af59827aa2bb9e6eafc6deda47c96aacdcb
RickWeber <863114+RickWeber@users.noreply.github.com>
2022-06-27 12:30:17 -0400

    Set up data download script

---
+++ 01_import_data.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+import io
+import zipfile
+
+url = "https://www.sec.gov/opa/data/market-structure/marketstructuredownloadshtml-by_security"
+page = requests.get(url).content
+soup = BeautifulSoup(page, 'html.parser')
+
+zip_links = [l.get('href') for l in soup.find_all('a', href=True) if re.search('zip', str(l))]
+
+zip_base = 'https://www.sec.gov/'
+
+def link_to_df(link):
+    r = requests.get(link)
+    b = io.BytesIO(r.content)
+    z= zipfile.ZipFile(b)
+    keep = [f.filename for f in z.filelist if re.search('csv', f.filename)][0]
+    path = z.extract(keep)
+    return pd.read_csv(path)
+
+df = link_to_df(zip_base + zip_links[0])
+
+for z in zip_links[1:]:
+    this_df = link_to_df(zip_base + z)
+    df = pd.concat([df, this_df])
